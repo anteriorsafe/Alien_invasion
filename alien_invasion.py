00d14aa149886ed91904fb45d9aeb418bd9a3b41
AnteriorSafe <edu.vasquezme@gmail.com>
2020-06-04 01:56:49 +0200

    Same first commint but the ship now moves :D

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion:
     """Overall call to manage game assets and behavior"""
 
@@ -20,6 +21,7 @@ class AlienInvasion:
         """start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             """Make the most recently drawn screen visible"""
             pygame.display.flip()
@@ -31,8 +33,16 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Redraw the screen during each pass through the loop."""
