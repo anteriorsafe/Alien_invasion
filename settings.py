0f8b90ba3866fdaef23f9629472e3bc88444e7a6
AnteriorSafe <edu.vasquezme@gmail.com>
2020-06-03 23:55:25 +0200

    First commit for Alien Invasion

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initilize the game settings"""
+        # Screen settings
+        self.screen_height = 800
+        self.screen_width = 600
+        self.bg_color = (230, 230, 230)
